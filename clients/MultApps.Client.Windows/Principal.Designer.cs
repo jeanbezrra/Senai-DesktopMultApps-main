0c02cfbda7c4e88c4d6541afb0a9a75e0b3e9ea5
Jean Bezerra <jeanbezrra@gmail.com>
2022-09-06 21:41:37 -0300

    Inicializando MultApps

---
+++ clients/MultApps.Client.Windows/Principal.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MultApps.Client.Windows
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.btnAbrirFormMatriculaAluno = new System.Windows.Forms.Button();
+            this.btnAbrirFormPesoIdeal = new System.Windows.Forms.Button();
+            this.btnAbrirFormReajusteSalarial = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbrirFormMatriculaAluno
+            // 
+            this.btnAbrirFormMatriculaAluno.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbrirFormMatriculaAluno.Font = new System.Drawing.Font("Stylus BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAbrirFormMatriculaAluno.Image = ((System.Drawing.Image)(resources.GetObject("btnAbrirFormMatriculaAluno.Image")));
+            this.btnAbrirFormMatriculaAluno.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnAbrirFormMatriculaAluno.Location = new System.Drawing.Point(48, 52);
+            this.btnAbrirFormMatriculaAluno.Name = "btnAbrirFormMatriculaAluno";
+            this.btnAbrirFormMatriculaAluno.Size = new System.Drawing.Size(167, 93);
+            this.btnAbrirFormMatriculaAluno.TabIndex = 0;
+            this.btnAbrirFormMatriculaAluno.Text = "Matricula Aluno";
+            this.btnAbrirFormMatriculaAluno.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAbrirFormMatriculaAluno.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnAbrirFormMatriculaAluno.UseVisualStyleBackColor = true;
+            this.btnAbrirFormMatriculaAluno.Click += new System.EventHandler(this.btnAbrirFormMatriculaAluno_Click);
+            // 
+            // btnAbrirFormPesoIdeal
+            // 
+            this.btnAbrirFormPesoIdeal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbrirFormPesoIdeal.Font = new System.Drawing.Font("Stylus BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAbrirFormPesoIdeal.Image = ((System.Drawing.Image)(resources.GetObject("btnAbrirFormPesoIdeal.Image")));
+            this.btnAbrirFormPesoIdeal.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnAbrirFormPesoIdeal.Location = new System.Drawing.Point(233, 52);
+            this.btnAbrirFormPesoIdeal.Name = "btnAbrirFormPesoIdeal";
+            this.btnAbrirFormPesoIdeal.Size = new System.Drawing.Size(167, 93);
+            this.btnAbrirFormPesoIdeal.TabIndex = 1;
+            this.btnAbrirFormPesoIdeal.Text = "Peso Ideal";
+            this.btnAbrirFormPesoIdeal.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAbrirFormPesoIdeal.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnAbrirFormPesoIdeal.UseVisualStyleBackColor = true;
+            this.btnAbrirFormPesoIdeal.Click += new System.EventHandler(this.btnAbrirFormPesoIdeal_Click);
+            // 
+            // btnAbrirFormReajusteSalarial
+            // 
+            this.btnAbrirFormReajusteSalarial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbrirFormReajusteSalarial.Font = new System.Drawing.Font("Stylus BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAbrirFormReajusteSalarial.Image = ((System.Drawing.Image)(resources.GetObject("btnAbrirFormReajusteSalarial.Image")));
+            this.btnAbrirFormReajusteSalarial.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnAbrirFormReajusteSalarial.Location = new System.Drawing.Point(415, 52);
+            this.btnAbrirFormReajusteSalarial.Name = "btnAbrirFormReajusteSalarial";
+            this.btnAbrirFormReajusteSalarial.Size = new System.Drawing.Size(167, 92);
+            this.btnAbrirFormReajusteSalarial.TabIndex = 2;
+            this.btnAbrirFormReajusteSalarial.Text = "Reajuste Salarial";
+            this.btnAbrirFormReajusteSalarial.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAbrirFormReajusteSalarial.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnAbrirFormReajusteSalarial.UseVisualStyleBackColor = true;
+            this.btnAbrirFormReajusteSalarial.Click += new System.EventHandler(this.btnAbrirFormReajusteSalarial_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(631, 206);
+            this.Controls.Add(this.btnAbrirFormReajusteSalarial);
+            this.Controls.Add(this.btnAbrirFormPesoIdeal);
+            this.Controls.Add(this.btnAbrirFormMatriculaAluno);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MultApps";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnAbrirFormMatriculaAluno;
+        private Button btnAbrirFormPesoIdeal;
+        private Button btnAbrirFormReajusteSalarial;
+    }
+}
---
+++ clients/MultApps.Client.Windows/Principal.cs
@@ -0,0 +1,39 @@
+﻿using MultApps.Client.Windows.Funcionalidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultApps.Client.Windows
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrirFormMatriculaAluno_Click(object sender, EventArgs e)
+        {
+            var form = new frmMatriculaAluno();
+            form.Show();
+        }
+
+        private void btnAbrirFormPesoIdeal_Click(object sender, EventArgs e)
+        {
+            var form = new frmPesoIdeal();
+            form.Show();
+        }
+
+        private void btnAbrirFormReajusteSalarial_Click(object sender, EventArgs e)
+        {
+            var form = new frmReajusteSalarial();
+            form.Show();
+        }
+    }
+}
